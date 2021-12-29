bad9823aee718c5494bb7afaa65e9cabb79f6984
LouNime <47531761+LouNime@users.noreply.github.com>
2021-12-29 16:06:48 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -145,7 +145,13 @@ function createMetaTag() {
   let head = select('head');
   meta.parent(head);
 }
-
+setCookie(cookie_name, "disabled", -1);
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
 window.setInterval(function() {
   var elem = document.getElementById('testouscita');
