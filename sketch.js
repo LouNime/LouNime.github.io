6be3dad68a71b2bd3756a8f7769089432a418aed
LouNime <47531761+LouNime@users.noreply.github.com>
2021-12-29 19:34:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -829,19 +829,19 @@ function DetunedOsc(){
   this.detune = 5;
 
   this.oscOne = new p5.Oscillator(midiToFreq(this.note),this.osctype);
-  this.oscTwo = new p5.Oscillator(midiToFreq(this.note),'sine');
+//  this.oscTwo = new p5.Oscillator(midiToFreq(this.note),'sine');
   this.oscOne.disconnect();
-  this.oscTwo.disconnect();
+  //this.oscTwo.disconnect();
   this.oscOne.start();
-  this.oscTwo.start();
+//  this.oscTwo.start();
 
   this.oscOne.connect(this.filter);
   //this.oscTwo.connect(this.filter);
 
   this.setNote = function(note){
       this.oscOne.freq(note);
-      this.oscTwo.freq(note/4);
-      this.oscOne.freq(this.oscTwo);
+    //  this.oscTwo.freq(note/4);
+    //  this.oscOne.freq(this.oscTwo);
   }
 
   this.setParams = function(params){
@@ -852,7 +852,7 @@ function DetunedOsc(){
   this.stop = function(){
     console.log("ci sono");
     this.oscOne.disconnect();
-    this.oscTwo.disconnect();
+  //  this.oscTwo.disconnect();
   }
   this.dispose = function(){
     delete this;
